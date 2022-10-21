fc1abf0ff6e7900875ae96bdd00fa3d005d00f9c
unknown <guilherme.cpmg@hotmail.com>
2022-10-21 17:05:54 -0300

    estudos

---
+++ src/pages/index.tsx
@@ -0,0 +1,41 @@
+import Layout from "../components/Layout"
+import Tabela from "../components/Tabela"
+import Cliente from "../core/Cliente"
+import Botao from "../components/Botao"
+export default function Home() {
+
+  const clientes = [
+    new Cliente('Ana', 34, "1"),
+    new Cliente('Gui', 27, "2"),
+    new Cliente('Dine', 28, "3"),
+    new Cliente('Lipe', 29, "4")
+  ]
+
+  function clienteSelecionado(cliente: Cliente) {
+    console.log(cliente.nome)
+  }
+
+  function clienteExcluido(cliente: Cliente) {
+    console.log(`Excluir ... ${cliente.nome}`)
+  }
+
+  return (
+    <div className={`
+      flex justify-center items-center h-screen
+      bg-gradient-to-r from-blue-500 to-purple-500
+      text-white
+    `}>
+      <Layout titulo="Cadastro">
+        <div className=" flex justify-end">
+        <Botao cor="green" className="mb-4" >
+                Novo Cliente
+        </Botao>
+        </div>
+        <Tabela clientes={clientes}
+          clienteSelecionado={clienteSelecionado}
+          clienteExcluido={clienteExcluido} />
+
+      </Layout>
+    </div>
+  )
+}
